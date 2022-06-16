5f184c052a793b46d4d9722e59b06025c18345be
Hussain <iffu041@gmail.com>
2022-06-16 04:43:11 +0000

    Created the project -Hussain

---
+++ src/main/java/com/hussain/learn/ProjectpensiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.hussain.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpensiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpensiondemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hussain/learn/ProjectpensiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussain.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpensiondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
